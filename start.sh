910ac2eda8bfe9ee9829b461a44d56a9ca75d074
Joshua Kukovec <i814u2@users.noreply.github.com>
2021-08-16 21:49:44 -0500

    Make sure executable and in the proper directory

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd /opt/udi-polyglotv2/
+
 if [[ ! -f /opt/udi-polyglotv2/polyglot-v2-linux-x64 ]]
 then
-	cd /opt/udi-polyglotv2/
 	wget -q https://s3.amazonaws.com/polyglotv2/binaries/polyglot-v2-linux-x64.tar.gz
 	tar -zxf polyglot-v2-linux-x64.tar.gz
+	chmod +x polyglot-v2-linux-x64
 fi
 
 ./polyglot-v2-linux-x64
